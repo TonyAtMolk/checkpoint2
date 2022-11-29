2e47efb9e17f38efb5de1e5708ae7bd9d048afa1
TonyAtMolk <76694144+TonyAtMolk@users.noreply.github.com>
2022-11-29 09:39:30 +0100

    Added comments

--- dict.py
+++ dict.py
@@ -6,23 +6,27 @@ conn = psycopg2.connect(
    port="5433",
    password="tmad1652"
 )
-
+# Fetch all data from database
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+# Soon deprecated. Replace with insert_word
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+# Insert a new word and translation to the 
 def insert_word(C, word, translation):
     print("Replacement function to add_word()")
+# Removes a translation from the database
 def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+# Commiting changes to the database
 def save_dict(C):
     cur = C.cursor()
     cur.execute("COMMIT;")
